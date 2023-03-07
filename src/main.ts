521049682e4f1df0c02e3c7906721c4b43759f44
Shai <shai.sultani@campus.technion.ac.il>
2023-03-07 12:20:57 +0200

    v3

--- src/main.ts
+++ src/main.ts
@@ -24,25 +24,9 @@ if (!city) {
 
 let db: Db;
 let channel: Channel;
-(async () => {
-    try {
-        const client = await MongoClient.connect(DB_URL);
-        db = client.db(DB_NAME);
-        console.log(`Connected to MongoDB ${DB_NAME}`);
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        process.exit(1);
-    }
-    try {
-        const conn = await connect(`amqp://guest:guest@localhost:${PUBLISHER_PORT}`);
-        channel = await conn.createChannel();
-        await channel.assertQueue(QUEUE_NAME);
-        console.log(`Connected to RabbitMQ queue ${QUEUE_NAME}`);
-    } catch (error) {
-        console.error('Error connecting to RabbitMQ:', error);
-        process.exit(1);
-    }
+let finished_publish = false;
 
+async function consumeMessages() {
     // Handle message from RabbitMQ
     const handleMessage = async (msg: any) => {
         const street: Street = JSON.parse(msg.content.toString());
@@ -55,11 +39,17 @@ let channel: Channel;
             console.error(error);
             channel.nack(msg);
         }
+        if (finished_publish){
+            console.error('Finished Successfully');
+            process.exit(1);
+        }
     };
 
     // Consume messages from RabbitMQ
     await channel.consume(QUEUE_NAME, handleMessage);
+}
 
+async function publishMessages() {
     // Publish streets to RabbitMQ
     const streets = await StreetsService.getStreetsInCity(city);
     for (const street of streets.streets) {
@@ -78,5 +68,30 @@ let channel: Channel;
         channel.sendToQueue(QUEUE_NAME, Buffer.from(message));
         console.log(`Message sent to queue: ${message}`);
     }
+    finished_publish=true;
     console.log(`Publish finished`);
+}
+
+(async () => {
+    try {
+        const client = await MongoClient.connect(DB_URL);
+        db = client.db(DB_NAME);
+        console.log(`Connected to MongoDB ${DB_NAME}`);
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+    try {
+        const conn = await connect(`amqp://guest:guest@localhost:${PUBLISHER_PORT}`);
+        channel = await conn.createChannel();
+        await channel.assertQueue(QUEUE_NAME);
+        console.log(`Connected to RabbitMQ queue ${QUEUE_NAME}`);
+    } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error);
+        process.exit(1);
+    }
+    // Start consuming messages in parallel
+    await consumeMessages();
+    // Start publishing messages in parallel
+    await publishMessages();
 })();
